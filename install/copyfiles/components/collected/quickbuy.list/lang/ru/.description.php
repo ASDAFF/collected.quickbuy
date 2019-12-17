29b95b9160cbba4b2b33daad06d7c297d4ee46c2
ASDAFF <asdaff.asad@yandex.ru>
2019-12-18 02:53:29 +0500

    start

---
+++ install/copyfiles/components/collected/quickbuy.list/lang/ru/.description.php
@@ -0,0 +1,6 @@
+<?
+$MESS['COLLECT_COM_NAME'] = "Успей купить";
+$MESS['COLLECT_COM_DESCRIPTION'] = "Компонент вывода товаров для модуля \"Успей купить\"";
+$MESS['COLLECT_COM_COMPONENTS'] = "ASDAFF";
+$MESS['COLLECT_COM_SEC_NAME'] = "Успей купить";
+?>
